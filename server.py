1f1255b18db7fe95eb459b3a6a7cc6271e2c8b87
risoletivo <anosderisos@gmail.com>
2025-01-17 09:23:28 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import asyncio
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from telethon import TelegramClient, events
 import os
 
@@ -23,6 +23,7 @@ async def handler(event):
         "text": event.message.text or "Sem texto",
         "date": event.message.date.strftime("%Y-%m-%d %H:%M:%S"),
         "media": bool(event.message.media),
+        "sender_id": event.sender_id,  # Adiciona o ID do remetente
     }
     messages_store.append(message_data)  # Adiciona a mensagem na memória
     print(f"Nova mensagem recebida: {message_data}")  # Log para ver no console
@@ -37,7 +38,16 @@ def index():
 @app.route('/getMessages', methods=['GET'])
 def get_messages():
     """Retorna mensagens armazenadas."""
-    return jsonify(messages_store)
+    bot_id = client.get_me().id  # Obtém o ID do bot
+    only_bot = request.args.get('only_bot', default=False, type=bool)
+
+    if only_bot:
+        # Filtra mensagens enviadas pelo bot
+        bot_messages = [msg for msg in messages_store if msg["sender_id"] == bot_id]
+        return jsonify(bot_messages)
+    else:
+        # Retorna todas as mensagens
+        return jsonify(messages_store)
 
 # Iniciar o cliente e o servidor Flask
 def main():
